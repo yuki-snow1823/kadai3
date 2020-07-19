5bc08e88214e80a368a552833d8f832a93de6db9
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-19 23:01:24 +0900

    add:comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.book_id = @book.id
+    comment.save
+    redirect_to book_path(@book)
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+end
