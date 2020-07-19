c55144eaa7b4a5a3328c2dd6848085caa3ec202c
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-19 22:10:04 +0900

    add:userモデルのバリデーションとアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
+  has_many :books, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 2..20 } 
+  validates :introduction, length: {maximum: 50}
 end
