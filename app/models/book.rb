3cbc6036b0d435218d894d4b5672b728bcb5ef8c
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-19 22:13:57 +0900

    add:bookモデルのアソシエーションの追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :body, presence: true, length: {maximum: 200}
+  has_many :post_comments, dependent: :destroy
 end
