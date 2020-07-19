584fe749ecebdc760973199bffea5aaaadcdd756
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-19 23:01:32 +0900

    add:book

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get books_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get books_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get books_show_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get books_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get books_destroy_url
+    assert_response :success
+  end
+
+end
