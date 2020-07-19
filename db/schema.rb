4d2a76f32ba3f0d89ecb76b360a145cbfded6cdb
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-19 21:42:48 +0900

    add:bookモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_123353) do
+ActiveRecord::Schema.define(version: 2020_07_19_123759) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title", default: "", null: false
+    t.string "body", default: "", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "profile_image_id"
